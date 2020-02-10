3fc19f7ceb025df7182188791fc1920f3a2e66e2
Alex Mielke <mielke.alexand@gmail.com>
2020-02-10 17:40:49 +0000

    arules_return bug fix

--- R/arules_return.R
+++ R/arules_return.R
@@ -1,6 +1,7 @@
 #' Prune and plot apriori ruleset
 #'
 #' Returns rules that satisfy specified 'support' (probability that A and B happen together) and 'confidence' (probability that B occurs if A is present) and plots them
+#' Rules work by selecting the lowest-order sufficient explanation for the observed data. So, for example, if the combination of A and B explains the occurrence of C, but A alone also explains C, than the rule is A -> C. If A alone does not explain C, and B alone neither, then AB -> C
 #'
 #' @param netfacs.data netfacs data list
 #' @param support probability that, in the data, the combination occurs
@@ -10,7 +11,7 @@
 #' @export
 #'
 #' @examples
-#' data(netfacstestdata)
+#' data(emotions_set)
 #' angry.face = netfacs(data = emotions_set[[1]],
 #'  condition = emotions_set[[2]]$emotion,
 #'  test.condition = 'anger',
@@ -20,7 +21,7 @@
 #'  control = NULL,
 #'  random.level = NULL,
 #'  combination.size = 5)
-#' arules.explorer(netfacs.data = netfacstestdata)
+#' arules.return(netfacs.data = angry.face, confidence = 0.8, support = 0.1)
 
 
 
@@ -28,12 +29,12 @@ arules.return <- function(netfacs.data, confidence = 0.8, support = 0.05){
   require(arules)
   require(arulesViz)
   
-  data = netfacs.data$arules
-  subRules = data[quality(data)$confidence>confidence & quality(data)$support>support]
+  netfacs.data = netfacs.data$arules # extract the arules object from the dataset
+  subRules = netfacs.data[quality(netfacs.data)$confidence>confidence & quality(netfacs.data)$support>support] #select those subrules that fulfil the selected criteria for support and confidence
   subset.rules = which(colSums(is.subset(subRules, subRules)) > 1)
   subRules = subRules[-subset.rules]
   
-  rule.list = list(ruleset = subRules, plot = plot(subRules, method = "graph",  engine = "htmlwidget"))
+  rule.list = list(ruleset = subRules, plot = plot(subRules, method = "graph",  engine = "htmlwidget")) # plot the rules as a html widget
   return(rule.list)
 
 }
