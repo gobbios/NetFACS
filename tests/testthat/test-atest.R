a35387dad5590c731f33cfe1c39b821dd35a1a19
Christof Neumann <christofneumann1@gmail.com>
2020-03-19 18:41:36 +0100

    delete test

--- tests/testthat/test-atest.R
+++
@@ -1,8 +0,0 @@
-test_that("reading data works", {
-  xdata <- read_mydata(path = system.file("anexamplefile.xlsx", package = "netfacstestr"))
-  xdata$context <- as.factor(xdata$context)
-
-  expect_equal(sum(xdata$AU1), expected = 2)
-  expect_true(nlevels(xdata$context) == 3)
-
-})
